6d1812ecb74ed42c3ddb166e6a9586729fc368a4
simonthi <github@simonthiefes.de>
2024-02-06 09:42:21 +0100

    Fonts updated

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-document.getElementById("magenta").addEventListener("click", function () {
-    document.querySelectorAll("body")[0].style.color = "magenta";
-    this.classList.add("active");
-    document.getElementById("black").classList.remove("active");
-    document.getElementById("olive").classList.remove("active");
-});
+window.addEventListener('load', () => {
+    document.getElementById("magenta").addEventListener("click", function () {
+        document.querySelectorAll("body")[0].style.color = "magenta";
+        this.classList.add("active");
+        document.getElementById("black").classList.remove("active");
+        document.getElementById("olive").classList.remove("active");
+    });
 
-document.getElementById("olive").addEventListener("click", function () {
-    document.querySelectorAll("body")[0].style.color = "olive";
-    this.classList.add("active");
-    document.getElementById("magenta").classList.remove("active");
-    document.getElementById("black").classList.remove("active");
-});
+    document.getElementById("olive").addEventListener("click", function () {
+        document.querySelectorAll("body")[0].style.color = "olive";
+        this.classList.add("active");
+        document.getElementById("magenta").classList.remove("active");
+        document.getElementById("black").classList.remove("active");
+    });
 
-document.getElementById("black").addEventListener("click", function () {
-    document.querySelectorAll("body")[0].style.color = "black";
-    this.classList.add("active");
-    document.getElementById("magenta").classList.remove("active");
-    document.getElementById("olive").classList.remove("active");
+    document.getElementById("black").addEventListener("click", function () {
+        document.querySelectorAll("body")[0].style.color = "black";
+        this.classList.add("active");
+        document.getElementById("magenta").classList.remove("active");
+        document.getElementById("olive").classList.remove("active");
+    });
 });
